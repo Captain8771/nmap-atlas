285061263373e2ef4d7c8662f13316f9c858d7b3
Niko <81252038+Captain8771@users.noreply.github.com>
2024-12-31 22:09:30 +0100

    g

--- src/utils.ts
+++ src/utils.ts
@@ -21,7 +21,6 @@ async function readFile() {
 
 async function writeFile(data: object) {
     let handle: fs.FileHandle = await fs.open(DATA_FILE, "w")
-    await handle.truncate()
     await handle.write(Buffer.from(JSON.stringify(data)))
     await handle.close()
 }
